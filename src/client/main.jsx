be8cf5a6082d09deecbbc2c90871d10968ff2d9f
Johnathan Williams <jlwill86@gmail.com>
2024-01-11 14:36:47 -0500

    intial commit

---
+++ src/client/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+// import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider>
+    <App />
+    </Provider>
+  </React.StrictMode>
+)
